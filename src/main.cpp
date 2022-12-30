e88bcc741c5fdf7c17a11c82a9c36807edc47d5c
Psylops <57834013+Psylops@users.noreply.github.com>
2022-12-30 18:53:30 +0000

    new file:   build/CMakeFiles/Progress/1 	new file:   build/CMakeFiles/Progress/10

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,22 @@
  
 int main(void) {
   using namespace ftxui;
- 
+  float percentage = 0;
+
+ for (int i; i<11; i++){
+    percentage += 0.1;
+ }
   // Define the document
   Element document =
     hbox({
-      border(gauge(0.5)) | border
+      border(gauge(percentage)) | border
+      Toggle();
     });
- 
   auto screen = Screen::Create(
     Dimension::Full(),       // Width
     Dimension::Fit(document) // Height
   );
   Render(screen, document);
   screen.Print();
- 
   return 0;
 }
